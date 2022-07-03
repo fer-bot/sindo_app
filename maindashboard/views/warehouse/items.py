61e6a30e4f551e29e5e17c7d2436e6faa49a30ae
darelion <43228043+darelion@users.noreply.github.com>
2022-07-03 21:15:55 +0700

    Edit Button

--- maindashboard/views/warehouse/items.py
+++ maindashboard/views/warehouse/items.py
@@ -1,3 +1,6 @@
+from ast import Del
+from itertools import product
+import re
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponse
@@ -5,7 +8,8 @@ from django.template import loader
 from django.db.models import OuterRef, Subquery, Sum
 from django.db.models.functions import Coalesce
 
-from maindashboard.models import DeliveryParty, Marking, OrderItem, TransferInfo
+from maindashboard.models import DeliveryParty, Marking, OrderItem, TransferInfo, TransferLogisticDetail
+from maindashboard.views.warehouse.marking import marking
 
 
 def warehouse_items(request):
@@ -53,7 +57,7 @@ def warehouse_items_new(request):
         try:
             quantity_unit = request.POST["quantityUnit"]
             delivery_party = DeliveryParty.objects.get(
-                pk=request.POST["deliveryParty"])
+            pk=request.POST["deliveryParty"])
             marking = Marking.objects.get(pk=request.POST["marking"])
             product_name = request.POST["productName"]
             product_img = request.FILES["productImage"]
@@ -101,10 +105,56 @@ def warehouse_items_new(request):
 def warehouse_items_edit(request, item_id):
     if request.method == 'GET':
         item = get_object_or_404(OrderItem, pk=item_id)
-
+        deliveryParty = DeliveryParty.objects.order_by('name')
+        item.entry_date = item.entry_date.strftime("%Y-%m-%d")
+        selectedDeliveryParty = get_object_or_404(DeliveryParty, pk=item.delivery_party_id)
+        selectedMarking = get_object_or_404(Marking , pk = item.marking_id)
+        selectedTransferInfo = get_object_or_404(TransferInfo, pk = item.warehousing_number)
+        markings =  Marking.objects.order_by('name')
         context = {
-            'item': item
+            'item': item, 
+            'selectedTransferInfo' : selectedTransferInfo,
+            'deliveryParty' : deliveryParty, 
+            'selectedDeliveryParty' : selectedDeliveryParty,
+            'selectedMarking' : selectedMarking,
+            'markings' : markings
         }
         template = loader.get_template(
             'warehouse/warehouse_items/warehouse_items_edit.html')
         return HttpResponse(template.render(context, request))
+    elif request.method == 'POST':
+        quantity_unit = request.POST["quantityUnit"]
+        product_name = request.POST["productName"]
+        product_img = request.FILES.get("productImage",False)
+        entry_date = request.POST["entryDate"]
+        volume = request.POST["volume"]
+        weight = request.POST["weight"]
+        description = request.POST["description"]
+        order_quantity = request.POST["quantity"]
+
+        item = get_object_or_404(OrderItem, pk=item_id)
+
+
+        item.quantity_unit = quantity_unit
+        item.product_name = product_name
+        item.entry_date = entry_date
+        item.volume = volume
+        item.weight = weight
+        item.description = description
+
+        item.save()
+        transferInfoSelected = TransferInfo.objects.filter(order_item_id = item_id).first()
+        # print(transferInfoSelected.to_detail)
+        # newTransferInfo = TransferInfo(
+        #         from_detail=from_detail,
+        #         to_detail=to_detail,
+        #         order_item=newItem,
+        #         order_quantity=order_quantity,
+        #         volume=volume,
+        #         weight=weight,
+        #         description="automated"
+        #     )
+        messages.info(
+                request, f'Adding Item with ID is successful!')
+        return redirect('/warehouse/items')
+
